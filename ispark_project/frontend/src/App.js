b13db4fe4f1250abc947286310d8059c34a4e7ad
AlperenCivelekoglu <alperencvlk65@gmail.com>
2024-08-31 10:25:45 +0300

    web önyüz harita tamamlandı
    el ile tanımlanılan marker lar yerine get isteği ile api den alınan veriler marker olarak haritaya eklendi.

--- ispark_project/frontend/src/App.js
+++ ispark_project/frontend/src/App.js
@@ -1,19 +1,35 @@
+import React, { useState, useEffect } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import { Icon, divIcon, point } from "leaflet";
 import MarkerClusterGroup from "react-leaflet-cluster";
-import "leaflet/dist/leaflet.css"
+import "leaflet/dist/leaflet.css";
 import "./App.css";
 
 export default function App() {
+  const [markers, setMarkers] = useState([]);
 
-  // create custom icon
+  useEffect(() => {
+    // API'ye GET isteği yapıyoruz
+    fetch("http://localhost:8000/api/parkingspot/")
+      .then((response) => response.json())
+      .then((data) => {
+        // JSON'dan veriyi alıp, state'e atıyoruz
+        const fetchedMarkers = data.features.map((feature) => ({
+          geocode: [feature.geometry.coordinates[1], feature.geometry.coordinates[0]], // LatLng sırasını düzeltelim
+          popUp: feature.properties.park_name // Pop-up'da park_name gösterebiliriz, ya da başka bir alan
+        }));
+        setMarkers(fetchedMarkers);
+      })
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
+  // Custom icon oluşturuyoruz
   const customIcon = new Icon({
     iconUrl: "https://cdn-icons-png.flaticon.com/512/447/447031.png",
-    //iconUrl: require("./icons/placeholder.png"),
-    iconSize: [38, 38] // size of the icon
+    iconSize: [38, 38] // Icon boyutu
   });
 
-  // custom cluster icon
+  // Custom cluster icon
   const createClusterCustomIcon = function (cluster) {
     return new divIcon({
       html: `<span class="cluster-icon">${cluster.getChildCount()}</span>`,
@@ -22,71 +38,23 @@ export default function App() {
     });
   };
 
-  // markers
-  const markers = [
-    {
-      geocode: [41.0488233569155, 28.9879749441319],
-      popUp: "Hello, I am pop up 1"
-    },
-    {
-      geocode: [41.0488233569155, 28.9879749441319],
-      popUp: "Hello, I am pop up 2"
-    },
-    {
-      geocode: [41.0495232987717, 28.9955656052444,],
-      popUp: "Hello, I am pop up 3"
-    }
-  ];
-
   return (
-    <MapContainer center={[41.05566, 28.9922]} zoom={13} style={{ height: "100vh", width: "100%" }}>
+    <MapContainer center={[41.05566, 28.9922]} zoom={13} style={{ height: "60vh", width: "60%" }}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"  // Tile URL'sini burada belirtin
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" 
       />
 
       <MarkerClusterGroup 
-      chunkedLoading
-      iconCreateFunction={createClusterCustomIcon}
+        chunkedLoading
+        iconCreateFunction={createClusterCustomIcon}
       >
-
-      {markers.map((marker) => (
-        <Marker position={marker.geocode} icon={customIcon}>
-          <Popup>{marker.popUp}</Popup>
-        </Marker>
-      ))}
-
+        {markers.map((marker, index) => (
+          <Marker key={index} position={marker.geocode} icon={customIcon}>
+            <Popup>{marker.popUp}</Popup>
+          </Marker>
+        ))}
       </MarkerClusterGroup>
     </MapContainer>
   );
 }
-// import React, { useEffect, useState } from 'react';
-// import axios from 'axios';
-
-// function App() {
-//   const [data, setData] = useState([]);
-
-//   useEffect(() => {
-//     axios.get('http://localhost:8000/api/parkingspot/')
-//       .then(response => {
-//         console.log(response.data);
-//         setData(response.data);
-//       })
-//       .catch(error => {
-//         console.error("There was an error fetching the data!", error);
-//       });
-//   }, []);
-
-//   return (
-//     <div>
-//       <h1>Parking Spots</h1>
-//       <ul>
-//         {data.map(item => (
-//           <li key={item.id}>{item.park_name} - {item.location_name}</li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// }
-
-// export default App;
